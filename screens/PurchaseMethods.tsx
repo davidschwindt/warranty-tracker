c9d5edea577f02d68c5d08b56f851e0d5bbc943b
jrconsole <jrconsole@gmail.com>
2022-12-09 03:02:31 -0600

    style purchase methods

--- screens/PurchaseMethods.tsx
+++ screens/PurchaseMethods.tsx
@@ -1,33 +1,65 @@
 import { useContext } from 'react';
 import { StyleSheet, Pressable } from 'react-native';
+import { SvgXml } from 'react-native-svg';
 
-import { Text, View } from '../components/Themed';
+import { Text, useThemeColor, View } from '../components/Themed';
 import { AppData } from '../data/Provider';
 import { RootStackScreenProps } from '../types';
+import CreditCardIcon from '../assets/icons/creditCard.svg';
 
 export default function PurchaseMethods({
   navigation,
 }: RootStackScreenProps<'PurchaseMethods'>) {
   const { purchaseMethods } = useContext(AppData);
+  const color = useThemeColor({}, 'text');
   return (
     <View style={styles.container}>
+      <Pressable onPress={() => navigation.goBack()} style={styles.link}>
+        <Text style={styles.linkText}>{'<-- Back'}</Text>
+      </Pressable>
       <Text style={styles.title}>Purchase Methods</Text>
       {Object.values(purchaseMethods).map(({ id, description }) => (
-        <Pressable onPress={() => navigation.navigate('AddMethod', { id })}>
-          <Text>{description}</Text>
-        </Pressable>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <SvgXml
+            xml={CreditCardIcon}
+            width={60}
+            height={60}
+            stroke={color}
+            style={{ marginRight: 8 }}
+          />
+          <Pressable onPress={() => navigation.navigate('AddMethod', { id })}>
+            <Text style={styles.text}>{description}</Text>
+          </Pressable>
+        </View>
       ))}
       <Pressable
         onPress={() => navigation.navigate('AddMethod')}
-        style={styles.link}
+        style={{ flexDirection: 'row', alignItems: 'center' }}
       >
+        <View
+          style={{
+            width: 60,
+            height: 60,
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginRight: 8,
+          }}
+        >
+          <View
+            style={{
+              borderColor: color,
+              borderWidth: 1.5,
+              borderRadius: 4,
+              width: '70%',
+              height: '45%',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Text style={{ fontWeight: '500', fontSize: 18 }}>+</Text>
+          </View>
+        </View>
         <Text style={styles.linkText}>Add a purchase method</Text>
-      </Pressable>
-      <Pressable
-        onPress={() => navigation.navigate('Home')}
-        style={styles.link}
-      >
-        <Text style={styles.linkText}>Go to home screen!</Text>
       </Pressable>
     </View>
   );
@@ -36,9 +68,9 @@ export default function PurchaseMethods({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
+    paddingHorizontal: 24,
+    paddingTop: 64,
+    paddingBottom: 48,
   },
   title: {
     fontSize: 20,
@@ -49,7 +81,10 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
   },
   linkText: {
-    fontSize: 14,
+    fontSize: 18,
     color: '#2e78b7',
+  },
+  text: {
+    fontSize: 18,
   },
 });
